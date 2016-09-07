d25353f68bd3a547045585f128a5aa1865e2d6ac
mam <mam@youzu.com>
2016-09-07 10:33:04 +0800

    add demos

---
+++ java/demos/d1/src/samples/Cat.java
@@ -0,0 +1,15 @@
+package samples;
+
+/**
+ * Created by maming on 16/6/23.
+ */
+public class Cat implements Animal {
+
+    public void run() {
+        System.out.println("samples.Cat run");
+    }
+
+    public void say() {
+        System.out.println("miaomiaomiao~~");
+    }
+}
